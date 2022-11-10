cae4cd214a3622aa4b037e3aec3f96462af70a3c
Sara Rocha de Oliveira <sararocha878@gmail.com>
2022-11-09 23:04:33 -0300

    chore: add routes status

--- src/server.js
+++ src/server.js
@@ -143,6 +143,24 @@ app.get('/messages', async (req, res) => {
 
 });
 
+app.post('/status', async (req, res) => {
+  try {
+    const { user } = req.headers;
+
+    const users = await db.collection("participants").findOne({ name: user });
+
+    if (!users) {
+      return res.sendStatus(404);
+    }
+
+    db.collection("participants").updateOne({ name: user }, { $set: { lastStatus: Date.now() } });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(404);
+  }
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(5000, () => {
